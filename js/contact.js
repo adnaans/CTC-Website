3d5d37c8aaa966ccc2f4d201645f449f7bfd6423
Zain Merchant <zainmerchant00@yahoo.com>
2017-04-10 16:20:23 -0700

    cleaned pages

--- js/contact.js
+++ js/contact.js
@@ -78,7 +78,7 @@ $('#name').focus(function() {
 
 
 function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        var uluru = {lat: 13, lng: 80};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: uluru
