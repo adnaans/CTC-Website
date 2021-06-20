e7ab072971623c9d12d49ad7f707e7b215f2f9a5
Sachi <sachi.sambandan@gmail.com>
2021-06-20 13:50:21 -0700

    moved latest release to last release branch

--- README.md
+++
@@ -1,2 +0,0 @@
-Website for ChallengeToConquer, a nonprofit that is gathering funds for Mithra Rehabilitation Centre.
-Mithra is a hospital for the mentally and physically disabled in India.
--- js/header.js
+++ js/header.js
@@ -1,63 +1,13 @@
-/*$(document).ready(function(){
-    $('#1-t').click(function (event){
-        $('.sel').addClass('u-sel');
-        $('.sel').removeClass('sel');
-        $('#1-t').addClass('sel');
-        $('#1-t').removeClass('u-sel');
-        $('.active-tw').addClass('inactive-tw');
-        $('.active-tw').removeClass('active-tw');
-        $('#1-tw').addClass('active-tw');
-        $('#1-tw').removeClass('inactive-tw');
-        
-    });
-    $('#2-t').click(function (event){
-        $('.sel').addClass('u-sel');
-        $('.sel').removeClass('sel');
-        $('#2-t').addClass('sel');
-        $('#2-t').removeClass('u-sel');
-        $('.active-tw').addClass('inactive-tw');
-        $('.active-tw').removeClass('active-tw');
-        $('#2-tw').addClass('active-tw');
-        $('#2-tw').removeClass('inactive-tw');
-    });
-    $('#3-t').click(function (event){
-        $('.sel').addClass('u-sel');
-        $('.sel').removeClass('sel');
-        $('#3-t').addClass('sel');
-        $('#3-t').removeClass('u-sel');
-        $('.active-tw').addClass('inactive-tw');
-        $('.active-tw').removeClass('active-tw');
-        $('#3-tw').addClass('active-tw');
-        $('#3-tw').removeClass('inactive-tw');
-    });
-    $('#4-t').click(function (event){
-        $('.sel').addClass('u-sel');
-        $('.sel').removeClass('sel');
-        $('#4-t').addClass('sel');
-        $('#4-t').removeClass('u-sel');
-        $('.active-tw').addClass('inactive-tw');
-        $('.active-tw').removeClass('active-tw');
-        $('#4-tw').addClass('active-tw');
-        $('#4-tw').removeClass('inactive-tw');
-    });
-    $('#5-t').click(function (event){
-        $('.sel').addClass('u-sel');
-        $('.sel').removeClass('sel');
-        $('#5-t').addClass('sel');
-        $('#5-t').removeClass('u-sel');
-        $('.active-tw').addClass('inactive-tw');
-        $('.active-tw').removeClass('active-tw');
-        $('#5-tw').addClass('active-tw');
-        $('#5-tw').removeClass('inactive-tw');
-    });
-    $('#6-t').click(function (event){
-        $('.sel').addClass('u-sel');
-        $('.sel').removeClass('sel');
-        $('#6-t').addClass('sel');
-        $('#6-t').removeClass('u-sel');
-        $('.active-tw').addClass('inactive-tw');
-        $('.active-tw').removeClass('active-tw');
-        $('#6-tw').addClass('active-tw');
-        $('#6-tw').removeClass('inactive-tw');
-    });
-});*/
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#slider');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('#mainNav').css('background', '#2C3E50');
+       } else {
+          $('#mainNav').css('background', 'transparent');
+       }
+   });
+});
